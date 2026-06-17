a87cd321af237f2cdf58d2bd877213603423847b
Torres Maxime <84190090+Red0Solatorobo@users.noreply.github.com>
2026-06-17 22:50:41 +0200

    Add files via upload

---
+++ Site internet/crea_soiree.php
@@ -0,0 +1,88 @@
+<?php
+
+$servername = "webmmi-peda.iut-tarbes.fr";
+$username_db = "725cobont";
+$password_db = "HNTJqr88TmVH";
+$dbname = "725cobont_groupe_2_sae_203";
+
+// Connexion
+$conn = mysqli_connect($servername, $username_db, $password_db, $dbname);
+
+// Vérification
+if (!$conn) {
+    die("C'est la mierda : " . mysqli_connect_error());
+}
+
+
+// Récupération des données du formulaire
+
+
+//PARTIE CHOIX NOM SOIREE
+$nom_soirée = $_POST['nom_soiree'];
+
+
+//PARTIE CHOIX DES FILMS
+$films = [];
+
+if (isset($_POST['film1'])) {
+    $films[] = $_POST['film1'];
+}
+
+if (isset($_POST['film2'])) {
+    $films[] = $_POST['film2'];
+}
+
+if (isset($_POST['film3'])) {
+    $films[] = $_POST['film3'];
+}
+
+$liste_films = implode(", ", $films);
+
+
+//PARTIE CHOIX LIEUX
+
+$lieux = [];
+
+if (isset($_POST['lieu1'])) {
+    $lieux[] = $_POST['lieu1'];
+}
+
+if (isset($_POST['lieu2'])) {
+    $lieux[] = $_POST['lieu2'];
+}
+
+if (isset($_POST['lieu3'])) {
+    $lieux[] = $_POST['lieu3'];
+}
+
+$liste_lieux = implode(", ", $lieux);
+
+//PARTIE CHOIX DATE
+
+$date = $_POST['date'];
+
+
+//PARTIE CHOIX THEME
+
+$theme = $_POST['theme'];
+
+
+// Requête SQL
+$sql = "INSERT INTO soirée(nom_soirée, Liste_film, Liste_lieu, date_soirée, thème) VALUES ('$nom_soirée', '$liste_films', '$liste_lieux','$date', '$theme')"; 
+//$sql = "ALTER TABLE soirée MODIFY COLUMN id_lieu INT NOT NULL AUTO_INCREMENT PRIMARY KEY";
+
+// Exécution
+if (mysqli_query($conn, $sql)) {
+    echo "$liste_films";
+    sleep(3);
+    header("Location: index.html");
+    exit;
+
+} else {
+    echo "Il y a eu un problème, veuillez réessayer.";
+}
+
+// Fermeture de la connexion
+mysqli_close($conn);
+
+?>
